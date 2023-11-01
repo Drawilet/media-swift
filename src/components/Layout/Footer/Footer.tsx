5d5722ac12b7da6312c7357f809e655c659da884
Drawilet <drawiletdev@gmail.com>
2023-10-31 19:34:45 -0500

    feat: add footer

---
+++ src/components/Layout/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <footer className="footer footer-center p-4 bg-base-300 text-base-content mt-10">
+      <aside>
+        <p>
+          Copyright © 2023 - All right reserved by{" "}
+          <a href="https://drawilet.me" className="text-primary" target="_blank">@Drawilet</a>
+        </p>
+      </aside>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,12 +1,15 @@
-import React from "react";
 import { Component } from "../../../types/Component";
+import React from "react";
+
 import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
 
 const Layout: Component = ({ children }) => {
   return (
     <>
       <Header />
       <main className="px-10 py-2 ">{children}</main>
+      <Footer />
     </>
   );
 };
