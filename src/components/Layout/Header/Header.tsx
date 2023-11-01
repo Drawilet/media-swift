356b95d890e95634678a8477cfe1bc8425757c97
Drawilet <drawiletdev@gmail.com>
2023-10-31 22:06:15 -0500

    feat: add logo

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -1,11 +1,21 @@
 import { Component } from "../../../../types/Component";
 
+import Img from "next/image";
+
 const links = [{ label: "Why Us?", href: "/#why-us" }];
 
 const Header: Component = () => {
   return (
-    <header className="navbar bg-base-100 sticky top-0 z-40">
+    <header className="navbar bg-base-100 sticky top-0 z-40 px-5">
       <div className="flex-1">
+        <Img
+          src={"/android-icon-48x48.png"}
+          alt="Media Swift"
+          title="Media Swift"
+          width={48}
+          height={48}
+        />
+
         <a
           className="btn btn-ghost normal-case text-xl"
           target="/"
