48a71f2299bb8996ec4abe0bd01a3f5d8c01ac95
Drawilet <drawiletdev@gmail.com>
2023-10-31 19:22:20 -0500

    fix: icons do not load

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,7 @@ export default function Document() {
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
           integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+          {...{ crossOrigin: "anonymous", referrerPolicy: "no-referrer" }}
         />
       </Head>
       <body className="bg-base-100">
