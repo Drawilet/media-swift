30defae669c4fbc97c314f57916b9f9a49e49211
Drawilet <drawiletdev@gmail.com>
2023-11-07 20:10:11 -0600

    feat: Add useFetcher hook for fetching data with loading state

---
+++ src/hooks/useFetcher.ts
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import { useLoading } from "./useLoading";
+
+const useFetcher = () => {
+  const { setLoading } = useLoading();
+
+  return async (input: RequestInfo, init?: RequestInit) => {
+    setLoading(true);
+
+    const response = await fetch(input, init);
+    let data;
+
+    const contentType = response.headers.get("content-type");
+    if (contentType?.includes("application/json")) data = await response.json();
+    else data = await response.blob();
+
+    setLoading(false);
+    return [response, data] as const;
+  };
+};
+
+export default useFetcher;
