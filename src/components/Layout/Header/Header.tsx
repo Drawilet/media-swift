c59fd869d1eaf2475d1cea8012b9168aecc6973d
Drawilet <drawiletdev@gmail.com>
2023-11-10 17:24:52 -0600

    feat: Update Header component with new styles and links

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -6,7 +6,7 @@ const links = [{ label: "Why Us?", href: "/#why-us" }];
 
 const Header: Component = () => {
   return (
-    <header className="navbar bg-base-100 sticky top-0 z-40 px-5">
+    <header className="navbar bg-base-100 sticky top-0 z-40 px-5 py-0 shadow-sm mb-3">
       <div className="flex-1">
         <Img
           src={"/android-icon-48x48.png"}
@@ -25,10 +25,10 @@ const Header: Component = () => {
         </a>
       </div>
       <div className="flex-none">
-        <ul className="menu menu-horizontal px-1">
+        <ul className="menu menu-horizontal px-1 justify-center">
           {links.map((link) => (
-            <li key={link.label}>
-              <a className="btn btn-ghost" href={link.href} title={link.label}>
+            <li key={link.label} >
+              <a href={link.href} title={link.label}>
                 {link.label}
               </a>
             </li>
