e84d341304a389412f37057697ebf6334110ec67
Drawilet <drawiletdev@gmail.com>
2023-11-10 21:33:41 -0600

    fixed: download large videos

--- src/pages/api/youtube/download.ts
+++ src/pages/api/youtube/download.ts
@@ -1,8 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import ytmux from "ytdl-core-muxer";
 import ffmpeg from "fluent-ffmpeg";
 import { path as ffmpegPath } from "@ffmpeg-installer/ffmpeg";
 import ytdl from "ytdl-core";
+import { spawn } from "child_process";
 
 ffmpeg.setFfmpegPath(ffmpegPath);
 
@@ -33,7 +33,46 @@ export default async function handler(
     res.setHeader("Content-Disposition", `attachment; filename="${filename}"`);
     res.setHeader("Content-Type", `${videoFormat.mimeType}`);
 
-    ytmux(videoUrl, { quality: videoFormat.itag }).pipe(res);
+    const ffmpegProcess = spawn(
+      ffmpegPath,
+      [
+        "-loglevel",
+        "8",
+        "-hide_banner",
+        "-i",
+        "pipe:3",
+        "-i",
+        "pipe:4",
+        "-map",
+        "0:a",
+        "-map",
+        "1:v",
+        "-c",
+        "copy",
+        "-f",
+        "matroska",
+        "pipe:5",
+      ],
+      {
+        windowsHide: true,
+        stdio: ["inherit", "inherit", "inherit", "pipe", "pipe", "pipe"],
+      }
+    );
+
+    if (!videoFormat.hasAudio) {
+      const audioStream = ytdl.downloadFromInfo(info, {
+        quality: "highestaudio",
+      });
+      audioStream.pipe(ffmpegProcess.stdio[3] as NodeJS.WritableStream);
+    }
+
+    const videoStream = ytdl.downloadFromInfo(info, {
+      format: videoFormat,
+    });
+    videoStream.pipe(ffmpegProcess.stdio[4] as NodeJS.WritableStream);
+
+    //@ts-ignore
+    (ffmpegProcess.stdio[5] as NodeJS.ReadableStream).pipe(res);
   } catch (error) {
     res.status(500).json({ error: "Failed to download video" });
   }
